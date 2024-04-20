45aa2d8929a680fd3057f3ec05b84b48c07df2e2
Garrett Moseke <gmoseke@protonmail.com>
2024-04-20 11:26:29 +0200

    feat: couple new convenience aliases in fish config
    I love you podman but I will never be able to use your CLI without typing "docker" I'm sorry

--- config.fish
+++ config.fish
@@ -5,8 +5,12 @@ end
 # starship
 starship init fish | source
 
+export PATH="$PATH:/opt/nvim/"
+
 # ===== Convenience Aliases =====
 # ensure lsd is installed: https://github.com/lsd-rs/lsd
 alias ls="lsd -l"
 alias code="codium"
 alias crabrave="open 'https://youtu.be/LDU_Txk06tM?t=28'"
+alias vim="nvim"
+alias docker="podman"
